4736854a2181d6bf9e9ab64846cb372ca8d99952
MercyAkpan <akpanmeme@gmail.com>
2024-08-28 12:57:33 +0100

    a
    '

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,8 @@
 import utils 
 import unittest
 from parameterized import parameterized
-
+from unittest.mock import patch, Mock
+import requests
 
 class TestAccessNestedMap(unittest.TestCase):
     """
@@ -26,6 +27,37 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(KeyError):
             utils.access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),  
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """
+        """
+        mock_reponse = Mock()
+        mock_reponse.json.return_value = test_payload
+        mock_get.return_value = mock_reponse
+
+        result = utils.get_json(test_url)
+        """
+            >>You should check the below first because it verifies that mock_get was used correctly.
+            If this assertion fails, it means the get_json function did not call mock_get as expected
+            and the subsequent assertion about the result may be irrelevant.
+        """
+        mock_get.assert_called_once_with(test_url)
+        """
+        >> If the mock interaction is correct, then you can be more confident 
+            that any failure in the result assertion 
+            is related to the function’s logic or result handling.
+        """
+        self.assertEqual(result, test_payload)
+
 
     if __name__ == "__main__":
         unittest.main()
